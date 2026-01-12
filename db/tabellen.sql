5d90e3193fd0f497d77e21d013564a58e4a10da0
8ogd4nn <bogdan.kudrenko@mng.ch>
2026-01-12 10:59:14 +0100

    Add additional columns to Spieler table

--- db/tabellen.sql
+++ db/tabellen.sql
@@ -1,3 +1,7 @@
 create table Spieler (
-  Spielername varchar(80),
-  NBA_Spielerwins int
+  Spielername varchar(80) primary key,
+  NBA_Spielerwins int,
+  Position varchar(80),
+  Alter int,
+  Grösse int,
+  Gewicht int,
