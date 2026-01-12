e6adf1d84d415d6e8c397bb84a05d01cebec7d34
8ogd4nn <bogdan.kudrenko@mng.ch>
2026-01-12 11:05:53 +0100

    Update tabellen.sql

--- db/tabellen.sql
+++ db/tabellen.sql
@@ -5,11 +5,25 @@ create table Spieler (
   Alter int,
   Grösse int,
   Gewicht int
-);
+  );
 create table Team (
   Teamname varchar(80) primary key,
   NBA_Teamwins int,
   City varchar(80),
   Coachname varchar(80)
   );
-create table
+create table Stats (
+  Statsid int primary key,
+  PPG float,
+  RPG float,
+  APG float,
+  SPG float,
+  BPG float,
+  FFG% float
+  );
+create table Spiel (
+  Spielid int primary key,
+  Score int,
+  Ort varchar(80),
+  Datum date
+  );
